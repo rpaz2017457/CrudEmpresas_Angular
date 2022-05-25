ca1ba7ac3241869188ccd91975c9e0c0be429350
Ricardo Benjamín Paz González <89035320+rpaz2017457@users.noreply.github.com>
2022-05-24 22:06:18 -0600

    Uploading Files

---
+++ src/app/models/empresas.model.ts
@@ -0,0 +1,11 @@
+export class Empresas {
+  constructor(
+    public _id: String,
+    public nombre: String,
+    public tipoEmpresa: String,
+    public password: String,
+    public rol: String,
+    public usuario: String,
+    public descripcion: String
+  ) {}
+}
---
+++ src/app/models/productos.model.ts
@@ -0,0 +1,10 @@
+export class Productos {
+  constructor(
+    public _id: String,
+    public NombreProducto: String,
+    public descripcion: String,
+    public NombreProveedor: String,
+    public Stock: Number,
+    public idEmpresa: String
+  ) {}
+}
